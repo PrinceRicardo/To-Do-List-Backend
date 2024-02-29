b21c9a9b04e2f35483ffc737c7df84b6dd3101ef
Sunetoss <simonsunetos0@gmail.com>
2024-02-29 14:19:50 -0400

    implement DB

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import express from "express";
+import{PORT, MONGODBURL } from './config.js'
+import { MongoClient, ServerApiVersion }from "mongodb"
 const app = express()
 
-const PORT = 3000
+
 
 app.use(express.json())
+
+const client = new MongoClient(MONGODBURL,  {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+}
+);
+
+const booksDB = client.db("myBookShop")
+const myBooks = booksDB.collection("books")
+
 app.listen(PORT, ()=> {
     console.log(`Server started on port ${PORT}`)
 })
@@ -20,3 +35,22 @@ app.get('/shop/:id',(req, res)=>{
     const data = req.params
     res.status(232).send(`<a href='/'> Book: ${data.id} </a>`)
 })
+
+app.post('/savebook',(req, res)=>{
+    const data = req.body
+
+    if(!data.title)
+        return res.status(400).send("No title found.")
+    if(!data.author)        
+        return res.status(400).send("No author found.")
+    if(!data.price)        
+        return res.status(400).send("No price found.")
+    myBooks.insertOne(data, (error, response)=>{
+        if(error){
+            console.log("An error occured!")
+            return res.sendStatus(500)
+        }
+    })
+    return res.status(201).send("Values are"+JSON.stringify(data))
+})
+
---
+++ config.js
@@ -0,0 +1,2 @@
+export const PORT = 3000
+export const MONGODBURL ="mongodb+srv://sunetosmongo:thisisapassword1@cluster0.eyiwwfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
