a6fcc1adf082f5763bb05ecc3b3dc2cbc1a410da
Charles White <ntrotman008@gmail.com>
2024-03-25 16:02:41 -0400

    first commit

--- app.js
+++ app.js
@@ -16,41 +16,55 @@ const client = new MongoClient(MONGODBURL,  {
 }
 );
 
-const booksDB = client.db("myBookShop")
-const myBooks = booksDB.collection("books")
+const toDoListDB = client.db("ToDoList")
+const myTasks = toDoListDB.collection("List")
 
 app.listen(PORT, ()=> {
     console.log(`Server started on port ${PORT}`)
 })
 
 app.get('/', (req, res)=> {
-    res.status(200).send("<h1> Sana sana colita de rana bish yeah!")
+    res.status(200).send("<h1> The one who left it all!")
 })
 
 app.get('/shop',(req, res)=>{
     res.status(232).send("<h1> A simple shop</h1>")
 })
 
-app.get('/shop/:id',(req, res)=>{
-    const data = req.params
-    res.status(232).send(`<a href='/'> Book: ${data.id} </a>`)
-})
 
-app.post('/savebook',(req, res)=>{
+app.post('/savetask', (req, res) => {
+    // Route adds a new book
     const data = req.body
+    if (!data.task)
+        return res.status(400).send("No task found.")
+    if (data.task.length > 160)
+    return res.status(400).send("limit exceeded (160)")
+    if (!data.visibility)
+        return res.status(400).send("No visibility found.")
+    if (!data.status)
+        return res.status(400).send("No status found.")
 
-    if(!data.title)
-        return res.status(400).send("No title found.")
-    if(!data.author)        
-        return res.status(400).send("No author found.")
-    if(!data.price)        
-        return res.status(400).send("No price found.")
-    myBooks.insertOne(data, (error, response)=>{
-        if(error){
-            console.log("An error occured!")
-            return res.sendStatus(500)
-        }
+    myTasks.insertOne(data)
+    .then(response=>{
+        return res.status(201).send(JSON.stringify(response))
     })
-    return res.status(201).send("Values are"+JSON.stringify(data))
+    .catch(err=>console.log(err))
 })
 
+app.get('/status/:id', (req, res) => {
+    // route show a specific book
+    const data = req.params
+
+    const filter = {
+        "_id": new ObjectId(data.id)
+        // "status" : "complete"
+    }
+
+    myTasks.findOne(filter)
+        .then(response => {
+            // console.log(response)
+            res.status(200).send(response)
+        })
+        .catch(err => console.log(err))
+    // return res.status(200).send(`<a href='/'> Book: ${data.id}</a>`)
+})
